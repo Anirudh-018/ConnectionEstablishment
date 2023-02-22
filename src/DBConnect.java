02f04b7e5db8c5be0c446ebcdd4f3027ba915cb4
Anirudh r k <menonani7@gmail.com>
2023-02-22 10:31:55 +0530

    GET USER DATA FOR INSERT AND SELECT

--- src/DBConnect.java
+++ src/DBConnect.java
@@ -1,6 +1,8 @@
 import java.sql.*;
+import java.util.*;
 public class DBConnect {
     Connection con=null;
+    Scanner s=new Scanner(System.in);
     void getConnection() throws Exception{
         con=DriverManager.getConnection("jdbc:mysql://localhost:3306/student", "root", "sairam123");
         System.out.println("connected succesfully");
@@ -14,9 +16,17 @@ public class DBConnect {
             System.out.println(result.getString("rollno")+" "+result.getString("name")+" "+result.getString("dob"));
         }
     }
+    void insertValues(String tablename) throws Exception{
+        Statement stmt=con.createStatement();
+        System.out.println("enter records");
+        String record=s.nextLine();
+        int affected=stmt.executeUpdate("insert into "+tablename+" values("+record+")");
+        System.out.println("Affected rows are :"+affected);
+    }
     public static void main(String[] args) throws Exception{
         DBConnect obj=new DBConnect();
         obj.getConnection();
         obj.retrieveData("details");
+        obj.insertValues("details");
     }
 }
